dad3c9f2d2e6e83276655ee504f24b891ee82d79
ce.jeong <ce.jeong@coga-robotics.com>
2024-12-18 15:35:46 +0900

    test

--- src/ojt_6/include/ojt_6/tf_sensor.h
+++ src/ojt_6/include/ojt_6/tf_sensor.h
@@ -3,27 +3,39 @@
 
 #include <ros/ros.h>  
 #include <sensor_msgs/LaserScan.h>  
-#include <sensor_msgs/PointCloud.h>  
-#include <tf2_ros/transform_listener.h>  
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/PointCloud2.h>
+// #include <sensor_msgs/PointCloud2Iterator.h>  
+#include "sensor_msgs/point_cloud2_iterator.h"
+// #include <tf2_ros/transform_listener.h>
+#include <tf/transform_listener.h>  
 #include <geometry_msgs/TransformStamped.h>  
 #include <tf/tf.h>  
 #include <cmath>  
 #include <iostream> 
 
+// std::vector<int> temp_vector;
+// std::vector<int>::iterator iter;
+// iter = temp_vector.begin();
+// for(iter; iter != temp_vector.end(); iter++)
+// {
+//      *iter = 
+// }
 
 class TFSensor{
     private:
     ros::NodeHandle n;
 
-    //todo lookuptransform tf 헤더에서 쓰기
-    tf2_ros::Buffer tfBuffer;  
-    tf2_ros::TransformListener tfListener; 
+    //todo lookuptransform tf 로 쓰기
+    // tf2_ros::Buffer tfBuffer;  
+    // tf2_ros::TransformListener tfListener; 
+    tf::TransformListener listener;
 
     ros::Subscriber sub_scan;
     ros::Subscriber sub_points;
 
     ros::Publisher pub_transform_scan;
-
+    ros::Publisher pub_transform_depth;
 
     // converCoordinate에 사용되는 변수
     float angle = 0.0f;
@@ -34,15 +46,15 @@ class TFSensor{
 
     // 회전 변환에 사용되는 변수
     // vertor<tuple<float, float, float>> convertXYZ;
-    geometry_msgs::TransformStamped transform_stamped;
-    
+    tf::StampedTransform transform_stamped_laser;
+    tf::StampedTransform transform_stamped_camera;
 
 
     void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
-    // void pointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
+    void pointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
 
     std::vector<tf::Vector3> convertCoordinate(const sensor_msgs::LaserScan::ConstPtr& msg);
-    tf::Vector3 TF(const geometry_msgs::TransformStamped& transform_stamped, tf::Vector3 original_v);
+    tf::Vector3 TF(const tf::StampedTransform& transform_stamped, tf::Vector3 original_v);
 
 
     public:
--- src/ojt_6/src/transformation_sensor.cpp
+++ src/ojt_6/src/transformation_sensor.cpp
@@ -1,13 +1,14 @@
 #include "ojt_6/tf_sensor.h"
 
-TFSensor::TFSensor() : tfListener(tfBuffer){
+TFSensor::TFSensor(){
 
-    sub_scan = n.subscribe("/scan", 10, &TFSensor::scanCallback, this);
-   // sub_points = n.subscribe("/camera/depth/points", 10, pointsCallback);
+    sub_scan = n.subscribe("/scan", 1, &TFSensor::scanCallback, this);
+    sub_points = n.subscribe("/camera/depth/points", 1, &TFSensor::pointsCallback, this);
 
-    pub_transform_scan = n.advertise<sensor_msgs::PointCloud>("/ojt/transform/scan2", 10);
-    //pub_transform_scan = n.advertise<geometry_msgs::PointStamped>("/ojt/transform/scan", 10);
-     printf("hi~2222/n");
+    pub_transform_scan = n.advertise<sensor_msgs::PointCloud>("/ojt/transform/scan", 10);
+    pub_transform_depth = n.advertise<sensor_msgs::PointCloud>("/ojt/transform/depth", 10);
+
+   // printf("hi~2222/n");
 }
 
 TFSensor::~TFSensor(){
@@ -16,31 +17,76 @@ TFSensor::~TFSensor(){
 
 void TFSensor::scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
   
-    transform_stamped = tfBuffer.lookupTransform("base_link", "laser", ros::Time(0));
+    // transform_stamped_laser = tfBuffer.lookupTransform("base_link", "laser_link", ros::Time(0));
+    listener.lookupTransform("base_link", "laser_link", ros::Time(0), transform_stamped_laser);
 
     // 극좌표계 -> 데카르트 좌표계
     std::vector<tf::Vector3> cartesian_points = convertCoordinate(msg);
 
     // 변환된 좌표계 데이터를 base_link 기준으로 TF 변환
-    tf::Vector3 transformed_point;
-    //pointCloud 변환할 때 사용할 변수
+    tf::Vector3 transformed_point_laser;
+    // pointCloud 변환할 때 사용할 변수
     geometry_msgs::Point32 p;
     sensor_msgs::PointCloud cloud_msg;
 
-    cloud_msg.header.stamp = msg->header.stamp;  // 시간 정보
-    cloud_msg.header.frame_id = "base_link";    // 변환된 프레임
+    cloud_msg.header.stamp = ros::Time::now();
+    cloud_msg.header.frame_id = "base_link";
 
-    for (tf::Vector3 point : cartesian_points){
-        transformed_point = TF(transform_stamped, point);  // TF 적용
 
-        p.x = transformed_point.x();
-        p.y = transformed_point.y();
-        p.z = transformed_point.z();
+    for(tf::Vector3 point : cartesian_points){
+        transformed_point_laser = TF(transform_stamped_laser, point);  // TF 적용
+
+        p.x = transformed_point_laser.x();
+        p.y = transformed_point_laser.y();
+        p.z = transformed_point_laser.z();
         cloud_msg.points.push_back(p);
     }
 
-
     pub_transform_scan.publish(cloud_msg);
+}
+
+/*
+
+
+ */
+void TFSensor::pointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg){
+    // TF 변환을 위한 TransformStamped 가져오기
+    // transform_stamped_camera = tfBuffer.lookupTransform("base_link", "camera_link", ros::Time(0));
+
+    listener.lookupTransform("base_link", "camera_link", ros::Time(0), transform_stamped_camera);
+    
+    // pointCloud 변환할 때 사용할 변수
+    geometry_msgs::Point32 p;
+    sensor_msgs::PointCloud camera_msg;
+
+    camera_msg.header.stamp = ros::Time::now();
+    camera_msg.header.frame_id = "base_link";  
+    
+    // todo pointcloud2 iterator xyz 접근 어케 함ㅜㅜ!!
+    sensor_msgs::PointCloud2ConstIterator<float> iter_x(*msg, "x");
+	sensor_msgs::PointCloud2ConstIterator<float> iter_y(*msg, "y");
+	sensor_msgs::PointCloud2ConstIterator<float> iter_z(*msg, "z");
+     
+    tf::Vector3 transformed_point_camera;
+
+    while(iter_x != iter_x.end()){
+        tf::Vector3 point(*iter_x, *iter_y, *iter_z);
+        tf::Vector3 transformed_point_camera = TF(transform_stamped_camera, point);
+
+            p.x = transformed_point_camera.x();
+            p.y = transformed_point_camera.y();
+            p.z = transformed_point_camera.z();
+
+            camera_msg.points.push_back(p); 
+
+            ++iter_x;
+            ++iter_y;
+            ++iter_z;
+    }
+    
+
+    // 변환된 PointCloud2 메시지 publish
+    pub_transform_depth.publish(camera_msg);
 }
 
 std::vector<tf::Vector3> TFSensor::convertCoordinate(const sensor_msgs::LaserScan::ConstPtr& msg){
@@ -61,12 +107,12 @@ std::vector<tf::Vector3> TFSensor::convertCoordinate(const sensor_msgs::LaserSca
     return cartesian_points;  // 변환된 좌표 목록 반환
 }
 
-tf::Vector3 TFSensor::TF(const geometry_msgs::TransformStamped& transform_stamped, tf::Vector3 original_v){
+tf::Vector3 TFSensor::TF(const tf::StampedTransform& transform_stamped, tf::Vector3 original_v){
     // TransformStamped에서 쿼터니언 가져오기
-    geometry_msgs::Quaternion geo_quat = transform_stamped.transform.rotation;
+    // geometry_msgs::Quaternion geo_quat = transform_stamped.transform.rotation;
 
     // 쿼터니언을 tf::Quaternion으로 변환
-    tf::Quaternion tf_quat(geo_quat.x, geo_quat.y, geo_quat.z, geo_quat.w);
+    tf::Quaternion tf_quat = transform_stamped.getRotation();
 
     // 회전 행렬 (tf::Matrix3x3) 생성
     tf::Matrix3x3 rotation_matrix(tf_quat);
@@ -74,9 +120,8 @@ tf::Vector3 TFSensor::TF(const geometry_msgs::TransformStamped& transform_stampe
     // 벡터 회전 변환
     tf::Vector3 rotated_v = rotation_matrix * original_v;
 
-    // 병진 변환(좌표계나 점을 특정 방향으로 일정한 거리만큼 평행 이동.) 적용
-    geometry_msgs::Vector3 translation = transform_stamped.transform.translation;
-    tf::Vector3 translated_v = rotated_v + tf::Vector3(translation.x, translation.y, translation.z);
+    // 회전 변환 + 직선 변환
+    tf::Vector3 translated_v = rotated_v + tf::Vector3(transform_stamped.getOrigin().x(), transform_stamped.getOrigin().y(), transform_stamped.getOrigin().z());
 
     return translated_v;
 }
@@ -108,10 +153,7 @@ tf::Vector3 TFSensor::TF(const geometry_msgs::TransformStamped& transform_stampe
 
 
 
-/*
-void TFSensor::pointsCallback(const sensor_msgs::PointCloud2::ConstPtr& msg){
 
-}
-*/
+
 
 
